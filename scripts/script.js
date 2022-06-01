ff30b83b7602569df5d38ae8e6bd420a19fbb228
AndeBurm <mer4end@gmail.com>
2022-06-01 20:42:03 +0300

    step 13, adaptivity step  4 include jQuery code LAST COMMIT I FUCKING HATE IT HERE

--- scripts/script.js
+++ scripts/script.js
@@ -23,13 +23,45 @@ $(function(){
     }
 
     /*Smooth scroll*/
-    $("data-scroll").on("click", function (event){
+    $("[data-scroll]").on("click", function (event){
         event.preventDefault();
 
-        var blockId = $(this).data('scroll'),
+        var $this = $(this),
+            blockId = $(this).data('scroll'),
             blockOffset = $(blockId).offset().top;
 
-        console.log(blockOffset);
+        $("nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+           scrollTop: blockOffset
+        }, 500);
     });
 
+    /*menu nav toggle*/
+
+    $("#nav_toggle").on("click", function (event){
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    /*collapse*/
+
+    $("[data-collapse]").on("click", function(event){
+        event.preventDefault();
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+    /*slider*/
+
+    $("[data-slider]").slick({
+       infinite: true,
+       fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
 });
